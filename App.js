b26eb41514ad4c2f1d727198ea8262c65d84a7e4
Ryan Atkinson <ryan@echobind.com>
2019-11-08 15:17:56 -0500

    react-navigation v5 updates

--- App.js
+++ App.js
@@ -6,8 +6,9 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+import React, {useState, Fragment} from 'react';
 import {
+  Alert,
   SafeAreaView,
   StyleSheet,
   ScrollView,
@@ -15,6 +16,9 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
+
+// what?
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import {
   Header,
@@ -24,55 +28,142 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App = () => {
+// 1 wrap entire app root
+import { NavigationNativeContainer } from '@react-navigation/native';
+
+// TABS
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// 2 import stack navigator util
+import { createStackNavigator } from '@react-navigation/stack';
+
+// 3 OPTION: native stack nav w/ less options and better perf
+import { useScreens } from 'react-native-screens';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// (3) req'd for native stack
+useScreens();
+
+const Tab = createBottomTabNavigator();
+
+// JS STACK
+// const Stack = createStackNavigator();
+
+// NATIVE STACK
+const Stack = createNativeStackNavigator();
+
+export const App = () => {
+  const [thirdTabName, setThirdTabName] = useState('Tab 🦅');
+
+  // new naviagtion prop "route"!
+  const MockStackScreen = ({ navigation, route }) => {
+    return (
+      <View style={{ flex: 1, width: '100%', justifyContent: 'space-around', alignItems: 'center' }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{route.name}</Text>
+        <View style={{ flexDirection: 'row', width: '100%', justifyContent: 'space-around', alignItems: 'center' }}>
+          {route.name !== 'Home' ? (
+            <TouchableOpacity
+              style={styles.navButton}
+              onPress={() => navigation.navigate('Home')}
+            >
+              <Text style={styles.navButtonText}>Go Home</Text>
+            </TouchableOpacity>
+          ) : null}
+          {route.name !== 'Profile' ? (
+            <TouchableOpacity
+              style={styles.navButton}
+              onPress={() => navigation.navigate('Profile')}
+              >
+                <Text style={styles.navButtonText}>Go to Profile</Text>
+            </TouchableOpacity>
+          ) : null}
+          {route.name !== 'Tabs' ? (
+            <TouchableOpacity
+              style={styles.navButton}
+              onPress={() => navigation.navigate('Tabs')}
+            >
+              <Text style={styles.navButtonText}>Go to Tabs</Text>
+            </TouchableOpacity>
+          ) : null}
+        </View>
+      </View>
+    );
+  }
+
+  const MockTabScreen = ({ navigation, route }) => {
+    return (
+      <View style={{ flex: 1, width: '100%', justifyContent: 'space-around', alignItems: 'center' }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{route.name}</Text>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => {
+            setThirdTabName('Tab 🍋');
+            navigation.navigate('Home');
+          }}
+        >
+          <Text style={styles.navButtonText}>Change Third Tab Name</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  const TabNav = () => {
+    return (
+      <Tab.Navigator
+        tabBarOptions={{
+          showIcon: false,
+          tabStyle: {
+            alignItems: 'center'
+          },
+          labelStyle: {
+            fontSize: 20,
+          },
+        }}
+      >
+        <Tab.Screen name="Tab 🔥" component={MockTabScreen} />
+        <Tab.Screen name="Tab ⛈️" component={MockTabScreen} />
+        <Tab.Screen name={thirdTabName} component={MockTabScreen} />
+      </Tab.Navigator>
+    );
+  };
+
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
+    <NavigationNativeContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        headerMode="screen"
+        screenOptions={{
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: 'tomato' },
+          title: "React Navigation 5 Example 🍃",
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={MockStackScreen}
+          options={{
+            title: "You're on the Home Screen",
+            headerTitleAlign: "center" // not working yet in docs... open-source :) ?
+          }} 
+        />
+        <Stack.Screen name="Profile" component={MockStackScreen} />
+        <Stack.Screen name="Tabs" component={TabNav} />
+      </Stack.Navigator>
+    </NavigationNativeContainer>
   );
 };
 
 const styles = StyleSheet.create({
+  navButton: {
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    padding: 12,
+    width: 120,
+  },
+  navButtonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
   scrollView: {
     backgroundColor: Colors.lighter,
   },
